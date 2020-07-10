1f1cdaf2efbe070c677dbe292f8f1c3fba57685e
raiguard <caleb.heuer@gmail.com>
2020-07-10 17:08:07 -0600

    Database setup

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
+require("dotenv").config();
 const express = require("express"),
+  massive = require("massive"),
   ctrl = require("./controller"),
+  { CONNECTION_STRING, SERVER_PORT, SESSION_SECRET } = process.env,
   app = express();
 
 app.use(express.json());
-
-const SERVER_PORT = 4000;
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false }
+}).then((db) => {
+  app.set("db", db);
+  console.log("Database conneciton established");
+});
 
 app.listen(SERVER_PORT, () => console.log(`Server is running on port ${SERVER_PORT}`));
+
+// TODO set up tables on DB side
